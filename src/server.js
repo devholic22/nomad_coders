dcd314637f40974e40ab337b80dc157ab759a340
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-11-17 22:33:04 +0900

    3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -1,21 +1,34 @@
+/* express use */
 import express from "express";
 
+/* PORT 4000 */
 const PORT = "4000";
 
+/* express save in app */
 const app = express();
 
+/* middleware function */
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
+/* response handle function */
 const handleHome = (req, res) => {
   return res.send("I still love you.");
 };
 
+/* response handle function */
 const handleLogin = (req, res) => {
   return res.send("Login here.");
 };
 
-app.get("/", handleHome);
+/* response "get (response)" statement */
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
+/* response "listen" statement */
 app.listen(4000, handleListening);
