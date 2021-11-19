d4a2094b284fda724b437d22ee82f07585b51692
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-11-19 23:05:47 +0900

    4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -13,22 +13,33 @@ const app = express();
 /* morgan middleware save in logger */
 const logger = morgan("dev");
 
-/* response handle function */
-const handleHome = (req, res) => {
-  return res.send("I still love you.");
-};
-
-/* response handle function */
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
 /* logger use */
 app.use(logger);
 
-/* response "get (response)" statement */
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+/* global router */
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+/* user router */
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+/* video router */
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
