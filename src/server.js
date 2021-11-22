5ef5aa43d398e0de576bea244b413d0a1b09bf00
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-11-22 22:48:13 +0900

    4.2 Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,41 +1,16 @@
-/* express use */
 import express from "express";
-
-/* morgan middleware use */
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
 
-/* PORT 4000 */
 const PORT = "4000";
 
-/* express save in app */
 const app = express();
 
-/* morgan middleware save in logger */
 const logger = morgan("dev");
 
-/* logger use */
 app.use(logger);
-
-/* global router */
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-/* user router */
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-/* video router */
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
@@ -44,5 +19,4 @@ app.use("/videos", videoRouter);
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-/* response "listen" statement */
 app.listen(4000, handleListening);
