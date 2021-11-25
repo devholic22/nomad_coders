0728b479628960c275eec395c449a61dab68c0c9
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-11-25 22:42:26 +0900

    4.7 URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ export const trending = (req, res) => res.send("Home Page Videos");
 export const see = (req, res) => {
   return res.send(`Watch Video #${req.params.id}`);
 };
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => {
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
