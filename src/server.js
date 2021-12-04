2caac200b77f379feeac690a7a947adc0c36042b
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-12-04 22:41:17 +0900

    6.11 Our First Query

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import "./db.js";
-import "./models/Video.js";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
-
-const PORT = "4000";
 
 const app = express();
 
@@ -20,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
-
-app.listen(4000, handleListening);
+export default app;
