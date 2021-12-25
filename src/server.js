7d115e1e87d2b027f1262ee96ca27fd37c01f3d1
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-12-26 00:22:25 +0900

    4.4 Router Recap

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+
 import globalRouter from "./routers/globalRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
@@ -8,6 +9,7 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
