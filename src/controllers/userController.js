069dea79b9f992262550beeafe882539c57a0265
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-02 15:15:57 +0900

    7.1 Create Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,19 @@
+import User from "../models/User.js";
+
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   console.log(req.body);
-  res.render("join", { pageTitle: "Join" });
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
