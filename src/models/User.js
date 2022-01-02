33fb77c36d6cfee0005fb4ac006baa44833d4280
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-02 15:54:42 +0900

    7.3 Form Validation

--- src/models/User.js
+++ src/models/User.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 5);
 });
+
 const userModel = mongoose.model("User", userSchema);
 
 export default userModel;
