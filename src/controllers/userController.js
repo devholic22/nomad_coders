e5236c64810394c80b137b3e9fbd319a50441c2d
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-08 18:48:29 +0900

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -127,6 +127,7 @@ export const finishGithubLogin = async (req, res) => {
       });
       req.session.loggedIn = true;
       req.session.user = user;
+      return res.redirect("/");
     }
   } else {
     return res.redirect("/login");
