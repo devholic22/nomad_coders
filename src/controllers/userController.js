77314ac2311c221293a1e383b9379479c30c6e3d
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-10 23:04:30 +0900

    8.7 File Uploads part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -136,9 +136,10 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
+    file,
   } = req;
   await User.findByIdAndUpdate(_id, {
     name,
@@ -149,6 +150,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
