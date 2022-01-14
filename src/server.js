d8002a5b393d6a8522326e99efdcc6d76e6ff3dc
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-14 22:30:44 +0900

    3.3 Responses

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ const PORT = 4000;
 
 const handleListening = () => console.log(`✅ Server listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+    return res.send("hello");
+}
 
 app.use("/", handleHome);
 
