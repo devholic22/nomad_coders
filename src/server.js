1ca37369158b16b1e273072e776d7236ec227f87
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-14 21:55:04 +0900

    3.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -5,4 +5,8 @@ const PORT = 4000;
 
 const handleListening = () => console.log(`✅ Server listening on: http://localhost:${PORT}`);
 
+const handleHome = (req, res) => console.log("Somebody is trying to go home.");
+
+app.use("/", handleHome);
+
 app.listen(PORT, handleListening);
