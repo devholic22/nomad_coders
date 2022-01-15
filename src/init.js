e324c3dd641cf82e212f4fb227f3a6946c127854
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-16 02:02:24 +0900

    6.14 Returns and Renders

--- src/init.js
+++ src/init.js
@@ -4,6 +4,7 @@ import Video from "./models/Video";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`✅ Server listening on: http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`✅ Server listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
-    hashtags: [{String}],
-    meta: {
-        views: Number,
-        rating: Number,
-    }
+  title: String,
+  description: String,
+  createdAt: Date,
+  hashtags: [{ String }],
+  meta: {
+    views: Number,
+    rating: Number
+  }
 });
 
 const Video = mongoose.model("Video", videoSchema);
