b4d9885c5a05d63114e9d8797d17e82be7d16402
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-16 02:19:45 +0900

    6.15 Creating a Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,6 +22,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(","),
+    meta: {
+      views: 0,
+      rating: 0
+    }
+  });
+  console.log(video);
   return res.redirect("/");
 };
