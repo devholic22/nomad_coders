354d55e263971e64cc8d31f519ab744ae168f09d
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-17 23:32:27 +0900

    7.5 Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/User";
 
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "create Account" });
+
 export const postJoin = async (req, res) => {
   const pageTitle = "Join";
   const { name, email, username, password, password2, location } = req.body;
@@ -18,17 +19,45 @@ export const postJoin = async (req, res) => {
       errorMessage: "⚠️ This username/email is already taken."
     });
   }
-  await User.create({
-    name,
-    email,
-    username,
-    password,
-    location
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password,
+      location
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("upload", {
+      pageTitle,
+      errorMessage: `❌ ${error._message}`
+    });
+  }
 };
-export const login = (req, res) => res.send("Login");
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const pageTitle = "Login";
+  const { username, password } = req.body;
+  const isExists = await User.exists({ username });
+  if (!isExists) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "⚠️ An account with this username does not exists."
+    });
+  }
+  // check if account exists
+  // check if password correct
+  res.end();
+};
+
 export const logout = (req, res) => res.send("Log out");
+
 export const edit = (req, res) => res.send("Edit User");
+
 export const remove = (req, res) => res.send("Remove User");
+
 export const see = (req, res) => res.send("See User");
