d014c306bddb471a62e5236cef8166c6c01f8bb4
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-22 01:07:00 +0900

    8.11 Video Owner

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,6 @@
 import Video from "../models/Video";
+import User from "../models/User";
+
 // import { formatHashtags } from "../models/Video";
 
 export const home = async (req, res) => {
@@ -9,10 +11,11 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  const owner = await User.findById(video.owner);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "❌ Video not found." });
   }
-  return res.render("watch", { pageTitle: video.title, video });
+  return res.render("watch", { pageTitle: video.title, video, owner });
 };
 
 export const getEdit = async (req, res) => {
@@ -58,6 +61,9 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const {
+    user: { _id }
+  } = req.session;
   const { path: fileUrl } = req.file; // es6 구문인데 공부해야 할 듯하다.
   const { title, description, hashtags } = req.body;
   try {
@@ -65,6 +71,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       fileUrl,
+      owner: _id,
       hashtags: Video.formatHashtags(hashtags)
     });
     return res.redirect("/");
