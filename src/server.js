a7eeb479f4ae41804691f05aea3ed2d78628f9e0
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-22 15:40:17 +0900

    9.3 Webpack Configuration part Three

--- src/server.js
+++ src/server.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads")); // 브라우저에게 폴더를 노출시킨다.
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
