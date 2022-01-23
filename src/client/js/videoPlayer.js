1523fe047ee9f0a4a7a7b6ea425015516c998de9
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-23 14:41:09 +0900

    11.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
@@ -19,9 +22,28 @@ const handleMute = (e) => {
   } else {
     video.muted = true;
   }
+  if (video.volume === 0) {
+    video.volume = 0.1;
+    volumeValue = 0.1;
+  }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value }
+  } = event;
+  volumeValue = Number(value);
+  video.volume = Number(value);
+  if (Number(value) === 0) {
+    video.muted = true;
+  } else {
+    video.muted = false;
+  }
+  muteBtn.innerText = Number(value) === 0 ? "Unmute" : "Mute";
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
