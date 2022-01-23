43ecd9e6fa94dae49d6527b30f2a1fba79074933
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-23 16:35:07 +0900

    11.4 Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -44,6 +45,22 @@ const handleVolumeChange = (event) => {
   muteBtn.innerText = Number(value) === 0 ? "Unmute" : "Mute";
 };
 
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+// JS에서 eventlistener를 추가하기 전에 video가 로딩이 되어
+// handleLoadedMetadate()가 아예 안 불러질 수 있으므로 아래 코드 추가
+if (video.readyState == 4) {
+  handleLoadedMetadata();
+}
