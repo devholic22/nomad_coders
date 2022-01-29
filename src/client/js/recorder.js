4d4b8edd11bee4204e0634c5ccf038ea55279450
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-29 22:34:30 +0900

    14.1 Transcode Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,7 +7,11 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "MyRecording.webm";
@@ -20,6 +26,7 @@ const handleStart = () => {
   recorder = new window.MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
+    console.log(videoFile);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
