6e2ff8b4d122ca3924a2b0e4332e89367df92436
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-01-29 22:52:55 +0900

    14.2 Download Transcoded Video

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -251,7 +251,6 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  console.log(user);
   // const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
     pageTitle: user.name,
