b798ba77dff50e8f41218dbd6afd1d8cea8db191
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-01 22:43:15 +0900

    2.4 Brush Size

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 
 const colors = document.getElementsByClassName("jsColor");
+const range = document.querySelector("#jsRange");
+const mode = document.querySelector("#jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -10,6 +12,7 @@ ctx.stokeStyle = "#2c2c2c";
 ctx.lineWidth = "2.5";
 
 let painting = false;
+let filling = false;
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
@@ -43,6 +46,29 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
