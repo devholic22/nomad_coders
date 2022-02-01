4bdb8321bf4d1526ee2f463ca1379797c2b0f7f2
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-01 12:56:27 +0900

    7.1 Adding To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input[type=text]");
 const toDoList = document.querySelector("#todo-list");
 
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  li.appendChild(span);
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   newTodo = toDoInput.value;
-  console.log(newTodo);
   toDoInput.value = "";
+  paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
