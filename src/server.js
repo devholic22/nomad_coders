c3cf07e4f43b68c695b79563838be6b0a4e0daba
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-22 12:15:50 +0900

    0.4 Recap

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 
 const app = express();
+const PORT = 3000;
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use("/public", express.static(process.cwd() + "/src/public"));
-app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListening = () =>
+  console.log(`✅ Server listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
