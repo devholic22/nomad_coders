e580f1dfff0aa381e15453b32f30962d28722f5d
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-22 15:45:07 +0900

    1.4 WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+// http에서의 url 접속 시 server.get("/") 처럼, ws에서의 접속으로 socket을 만들어낸다.
+const socketWithBack = new WebSocket(`ws://${window.location.host}`);
+
+// frontEnd webSocket이 backEnd와 연결되었다면 실행될 함수
+socketWithBack.addEventListener("open", () => {
+  console.log("✅ Connected to Server");
+});
+
+socketWithBack.addEventListener("message", (message) => {
+  console.log("Message from Server: ", message.data);
+});
+
+socketWithBack.addEventListener("close", () => {
+  console.log("❌ Disconnected to Server");
+});
+
+setTimeout(() => {
+  socketWithBack.send("hello from the browser");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -16,11 +16,17 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListening = () =>
   console.log(`✅ Server listening on: http://localhost:${PORT}`);
 
-const server = http.createServer(app);
+const server = http.createServer(app); // http 분리
+const wss = new WebSocket.Server({ server }); // ws 분리 (http와 같은 PORT)
 
-const wss = new WebSocket.Server({ server });
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+// backEnd webSocket이 frontEnd와 연결되었다면 실행될 함수
+wss.on("connection", (socketWithFront) => {
+  console.log("✅ Connected to Browser");
+  socketWithFront.on("close", () => console.log("❌ Disconnected to Browser"));
+  socketWithFront.on("message", (message) =>
+    console.log(message.toString("utf-8"))
+  );
+  socketWithFront.send("hello!");
+});
+
 server.listen(PORT, handleListening);
