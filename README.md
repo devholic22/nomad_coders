4a4cf9d4924076dd33d6961ccd5ae0c409c65d98
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-22 11:48:43 +0900

    [RESTART] 0.2 Server Setup

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# zoom_clone
-
-노마드코더의 줌 클론 강의 실습 레포지토리입니다. 향후 유튜브 클론에 적용시켜볼 계획입니다.
-
 # Noom
 
-Zoom Clone using NodeJS, WebRTC and Websockets.
+Zoom Clone using NodeJS, WebRTC and Websockets by Nomad Coders.
--- src/server.js
+++ src/server.js
@@ -1,34 +1,6 @@
-import http from "http";
-import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
+console.log("hello");
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-const server = http.createServer(app);
-const ws = new WebSocketServer({ server }); // http의 서버 위에 ws 서버를 올려둠으로써 같은 포트에서 운용할 수 있다.
-
-function onSocketClose() {
-  console.log("❌ Disconnected from the Browser");
-}
-
-const sockets = [];
-
-ws.on("connection", (backSocket) => {
-  sockets.push(backSocket);
-  console.log("✅ Connected to the Browser");
-  backSocket.on("close", onSocketClose);
-  backSocket.on("message", (message) => {
-    sockets.forEach((anySocket) => anySocket.send(message.toString()));
-    // backSocket.send(message.toString());
-  });
-});
-
-server.listen(3000, handleListen);
+app.listen(3000);
