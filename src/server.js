3358dfd121c77f90d277b5ff0b120e0adb28059e
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-22 12:05:12 +0900

    0.3 Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 
 const app = express();
-console.log("hello");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.use("/public", express.static(process.cwd() + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
 
 app.listen(3000);
