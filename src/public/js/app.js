abdeca530c936e393744e2e9cd7003283eb677f5
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-26 16:15:45 +0900

    3.1 Call Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -93,24 +93,43 @@ socketWithBack.on("room_change", (rooms) => {
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 const getMedia = async () => {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
+      audio: true,
       video: true
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (error) {
     console.log(error);
   }
 };
 
 const handleMuteClick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -121,6 +140,9 @@ const handleMuteClick = () => {
 };
 
 const handleCameraClick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
