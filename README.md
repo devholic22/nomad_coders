866cb9b782c27e1b44c3026cb73f6c29ddc0a20e
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-03-30 16:20:03 +0900

    0.2 Server Setup
    Zoom clone reboot

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Noom
 
 Zoom Clone using NodeJS, WebRTC and Websockets by Nomad Coders.
+
+## RESTART
+
+2022/03/30
--- src/server.js
+++ src/server.js
@@ -1,42 +1,7 @@
-import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
-const PORT = 3000;
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
+console.log("hello");
 
-app.use("/public", express.static(process.cwd() + "/src/public"));
-
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-
-const handleListening = () =>
-  console.log(`✅ Server listening on: http://localhost:${PORT}`);
-
-const httpServer = http.createServer(app); // http 분리
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true
-  }
-});
-
-wsServer.on("connection", (socketWithFront) => {
-  socketWithFront.on("join_room", (roomName, startMedia) => {
-    socketWithFront.join(roomName);
-    startMedia();
-    socketWithFront.to(roomName).emit("welcome");
-  });
-  socketWithFront.on("offer", (offer, roomName) => {
-    socketWithFront.to(roomName).emit("offer", offer);
-  });
-});
-instrument(wsServer, {
-  auth: false
-});
-
-httpServer.listen(PORT, handleListening);
+app.listen(3000);
