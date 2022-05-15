9e9eb83f51aa9a0a5563f2bc50d4d143e3747803
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-05-15 20:04:21 +0900

    1.2 WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -11,4 +13,9 @@ app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 
-app.listen(PORT, handleListen);
+// app.listen(PORT, handleListen);
+// http와 webSocket을 같이 쓰고 싶은 경우..
+const server = http.createServer(app);
+const wss = new WebSocket.WebSocketServer({ server });
+
+server.listen(PORT, handleListen);
