5ea507dad99a67b8d5e77ab5dac41c5c73bbef2e
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-05-26 02:04:45 +0900

    1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,6 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socketWithBack = new WebSocket(`ws://${window.location.host}`);
 
 // open event
@@ -9,7 +10,10 @@ socketWithBack.addEventListener("open", () => {
 
 // message event
 socketWithBack.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  // console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // close event
@@ -22,11 +26,24 @@ socketWithBack.addEventListener("close", () => {
 //  socketWithBack.send("hello from the browser!");
 // }, 10000);
 
-const handleSubmit = (event) => {
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+const handleNickSubmit = (event) => {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socketWithBack.send(input.value);
+  const input = nickForm.querySelector("input");
+  socketWithBack.send(makeMessage("nick", input.value));
   input.value = "";
 };
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socketWithBack.send(makeMessage("new_message", input.value));
+  input.value = "";
+};
+
+nickForm.addEventListener("submit", handleNickSubmit);
 messageForm.addEventListener("submit", handleSubmit);
